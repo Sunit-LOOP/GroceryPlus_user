c5841947be28a8840787f3abe00809ac56c3297b
Sunit Dangaura Tharu <chaudharyzayn725@gmail.com>
2025-12-29 12:04:40 +0545

    Version control: Update version to 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sunit.groceryplus"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.3.1"
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
