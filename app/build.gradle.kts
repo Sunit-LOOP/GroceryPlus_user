ae82e36535c8b0488837a7f47d7f19717c2654be
Sunit Dangaura Tharu <chaudharyzayn725@gmail.com>
2025-12-29 11:41:43 +0545

    Version 1.3.1: Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sunit.groceryplus"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
